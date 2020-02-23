0b91d754fd6630e663701cbb367b3d7a094e4b2d
Alexis <alexisophie.k@gmail.com>
2020-02-23 13:34:22 -0800

    final fixes

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,27 +1,13 @@
-#################################################
-# MongoDB and Flask Application
-#################################################
 
-# Dependencies and Setup
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import scrape
 
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
 
-#################################################
-# PyMongo Connection Setup
-#################################################
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-#################################################
-# Flask Routes
-#################################################
-# Root Route to Query MongoDB & Pass Mars Data Into HTML Template: index.html to Display Data
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
